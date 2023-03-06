05770ad9d33bd63ca11729be9a300ee8263c370e
Jingyi Xu <jx15@ualberta.ca>
2023-03-06 15:45:26 -0700

    done!

--- authlab/settings.py
+++ authlab/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'client.apps.ClientConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -128,5 +130,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     )
 }
